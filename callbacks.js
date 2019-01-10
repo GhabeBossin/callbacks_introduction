b776d570d751650794d4a1cc401fc3045d4432a4
Gabrielle Bossin <gee.bossin@gmail.com>
2019-01-10 22:03:20 +0000

    refactored to replace for loop with .forEach method

--- callbacks.js
+++ callbacks.js
@@ -1,20 +1,22 @@
 // Modify the callback function in the previous example so that logs the index of the array where Waldo is found, ie. "Found Waldo at index 2!". (You will need to modify actionWhenFound to receive the index.)
-
+//then
+//refactor code to include .forEach method instead of a for loop.
 
 // The second argument/parameter is expected to be a function
 
 var whereIsWaldo = "";
+//var nameArray = ["Alice", "Bob", "Waldo", "Winston"];
 
 function findWaldo(arr, found) {
-   for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === "Waldo") {
+	arr.forEach(function(name, i) {
+		if (name === "Waldo") {
 			whereIsWaldo = i;
 			found(); // execute callback
-      }
-   }
+		}
+	});
 }
 
-function actionWhenFound() {
+function actionWhenFound() { //cb
    console.log("Found Waldo at Index " + whereIsWaldo + "!");
 }
 
