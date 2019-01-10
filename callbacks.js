2769fb9639c9a433bc901b77c42b616018f98e58
Gabrielle Bossin <gee.bossin@gmail.com>
2019-01-10 21:26:59 +0000

    finished callback arguments portion

--- callbacks.js
+++ callbacks.js
@@ -1,12 +1,21 @@
-var myFn = function() {
-    console.log("I am a function.");
+// Modify the callback function in the previous example so that logs the index of the array where Waldo is found, ie. "Found Waldo at index 2!". (You will need to modify actionWhenFound to receive the index.)
+
+
+// The second argument/parameter is expected to be a function
+
+var whereIsWaldo = "";
+
+function findWaldo(arr, found) {
+   for (var i = 0; i < arr.length; i++) {
+      if (arr[i] === "Waldo") {
+			whereIsWaldo = i;
+			found(); // execute callback
+      }
+   }
 }
 
-myFn.someAttribute = 42;
-console.log(myFn.someAttribute);
-
-function runner(f) {
-    f();
+function actionWhenFound() {
+   console.log("Found Waldo at Index " + whereIsWaldo + "!");
 }
 
-runner(myFn);
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
